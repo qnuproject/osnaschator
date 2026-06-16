9fbf6891e106d93827608332a7143e7f7a542c00
qnuproject <asdfgdevv@gmail.com>
2026-06-16 15:42:56 -0400

    Instler

---
+++ build.sh
@@ -0,0 +1,19 @@
+# This is a part of the QNU Project.
+
+__QNU_UNIQUE_BUILD_SYSTEM_COMPILATOR="gcc"
+__QNU_UNIQUE_BUILD_SYSTEM_INSTALLPATH="/usr/local/bin"
+
+if command -v $__QNU_UNIQUE_BUILD_SYSTEM_COMPILATOR >/dev/null 2>&1; then
+  echo Building...
+  $__QNU_UNIQUE_BUILD_SYSTEM_COMPILATOR main.c -o osnaschator || echo ERROR && exit 1
+  echo Successfully built!
+  echo Do you want to install the utility? [y/n]
+  read -r otvet
+  if [ "$otvet" = "y" ]; then
+    mv osnaschator $__QNU_UNIQUE_BUILD_SYSTEM_INSTALLPATH/osnaschator
+  else
+    echo ERROR && exit 2
+  fi
+else
+  echo ERROR && exit 3
+fi
